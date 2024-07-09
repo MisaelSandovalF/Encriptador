04b53172f79494659b9d6081b5d9684d49a9be22
Misael <misaelS10@Outlook.com>
2024-07-09 16:08:20 -0600

    Encriptador V.0.1

---
+++ main.js
@@ -0,0 +1,49 @@
+let resizeBrowser = () =>  {
+    let widthBrowser = window.outerWidth;
+    console.log(widthBrowser);
+    
+    document.getElementById('muñeco').style.display = 'display';
+    if (widthBrowser < 1024 ){
+      
+        document.getElementById('output-text-area').style.display = 'none';
+        document.getElementById('muñeco').style.display = ' inline-block';
+        // document.getElementById('dinamic-container-id').style.display = 'flex';
+       
+    }
+    else if(widthBrowser > 1024){
+        // document.getElementById('output-text-area').style.display = 'flex';
+        // document.getElementById('muñeco').style.display = ' none';
+        document.getElementById('dinamic-container-id').style.display = ' none';
+    }
+   
+}
+
+
+const activeModal = () =>{
+        
+    const screenWidth = screen.width;
+    const breakpoint = 1024;
+
+    if (screenWidth < breakpoint) {
+        document.getElementById('dinamic-container-id').style.display = 'flex';
+       
+    }
+   
+    if (screenWidth > breakpoint) {
+       document.getElementById('output-text-area').style.display = 'flex';
+       document.getElementById('muñeco').style.display = 'none';
+
+   }
+  
+
+}
+
+const closeModal = () =>{
+ 
+    
+    document.getElementById('dinamic-container-id').style.display = 'none';
+}
+
+
+    
+
